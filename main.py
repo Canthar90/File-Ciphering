16c36a4bea699b64f06fb2c54f4348e4d75a50c6
Canthar90 <d.cieslak90@gmail.com>
2024-03-05 16:58:19 +0100

    Some tests but im unable to enable dragging and dropping directories so i will probably change Gui library

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import PySimpleGUI as sg
+from file_cyphering import EncryptFiles
+import os
+
+encrypter = EncryptFiles()
 
 layout = [
     [sg.Text('Drag and drop directories here:')],
-    [sg.Input(size=(50, 1), key='-FOLDER-'), sg.FolderBrowse()],
+    [sg.Input(size=(50, 1), key='-PATH-', enable_events=True), sg.FolderBrowse()],
     [sg.Button('Green',  button_color=('black' ,'#87A920')), sg.Button('Purple', button_color=('white', '#6420AA'))]
 ]
 
@@ -14,6 +18,8 @@ while True:
         break
     if event == 'Green':
         sg.popup('Green button clicked!')
+        path = values['-PATH-']
+        print(path)
     elif event == 'Purple':
         sg.popup('Purple button clicked!')
 
